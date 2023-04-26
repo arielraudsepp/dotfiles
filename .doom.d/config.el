df0c5c23fe9d81dfd871a0b41146ac3e0c0a4001
ariel <araud01@students.cs.ubc.ca>
2023-04-25 21:27:36 -0700

    Add blinders mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -81,3 +81,60 @@
                                             "Notes")
            "* %?\n %i\n"
            :empty-lines 1))))
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Blinders Mode
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defgroup blinders nil
+  "Put on your blinders so you can focus on one line at a time"
+  :group 'convenience)
+
+;; (defface blinders-hidden-face
+;;   (list (list t (list :background (face-attribute 'default :background)
+;;                       :foreground (face-attribute 'default :background))))
+;;   "blinders-mode hidden face"
+;;   :group 'blinders)
+
+(defcustom blinders-mode-timer-length 5
+  "Amount of time to wait until the blinders are removed"
+  :type 'integer
+  :group 'blinders)
+(defvar-local blinders-mode--timer nil)
+
+(defun blinders-mode--remove-blinders ()
+  (interactive)
+  (remove-overlays (point-min) (point-max) 'category 'blinders))
+
+(defun blinders-mode--refresh-blinders ()
+  (interactive)
+  ;; deal with timers
+  (when blinders-mode--timer
+    (cancel-timer blinders-mode--timer))
+  (setq-local blinders-mode--timer (run-with-timer blinders-mode-timer-length nil 'blinders-mode--remove-blinders))
+  ;; hide buffer
+  (let ((before-ov (make-overlay (point-min)
+                                 (line-beginning-position 0)))
+        (after-ov (make-overlay (line-end-position 2)
+                                (point-max))))
+    (blinders-mode--remove-blinders)
+    (overlay-put before-ov 'category 'blinders)
+    (overlay-put before-ov 'face 'blinders-hidden-face)
+    (overlay-put after-ov 'category 'blinders)
+    (overlay-put after-ov 'face 'blinders-hidden-face)))
+
+(define-minor-mode blinders-mode
+  "Mode for showing only one line of the buffer at a time"
+  :init-value nil
+  (if blinders-mode
+      (progn
+        (defface blinders-hidden-face
+          (list (list t (list :background (face-attribute 'default :background)
+                              :foreground (face-attribute 'default :background))))
+          "blinders-mode hidden face"
+          :group 'blinders)
+        (setq-local post-command-hook (cons 'blinders-mode--refresh-blinders post-command-hook)))
+    (progn
+      (setq-local post-command-hook (delete 'blinders-mode--refresh-blinders post-command-hook))
+      (blinders-mode--remove-blinders))))
+
+(add-hook 'prog-mode-hook 'blinders-mode)
