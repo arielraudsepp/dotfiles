0db1dc058b793a548fde391360e0de6d163429f3
Ariel Raudsepp <ariel.raudsepp@gmail.com>
2024-05-07 10:18:03 -0700

    use git-sync for org files

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -33,6 +33,9 @@
 (package! blinders-mode
   :recipe (:host github :repo "arielraudsepp/blinders-mode"))
 
+(package! git-sync-mode
+  :recipe (:host github :repo "justinbarclay/git-sync-mode"))
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
