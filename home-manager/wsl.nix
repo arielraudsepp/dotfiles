f1c12e998f390c04c380119cd5ccea9ebfa6def4
Ariel Raudsepp <ariel.raudsepp@gmail.com>
2024-05-16 09:03:22 -0700

    formatting changes

--- home-manager/wsl.nix
+++ home-manager/wsl.nix
@@ -48,24 +48,23 @@ with lib; {
         <dir>/mnt/c/Windows/Fonts</dir>
       '';
     };
-    i18n.defaultLocale= "en_CA.UTF-8";
+    i18n.defaultLocale = "en_CA.UTF-8";
     programs.zsh = {
       enable = true;
       shellAliases = {
-      #   ssh = "ssh.exe";
-      #   ssh-add = "ssh-add.exe";
+        #   ssh = "ssh.exe";
+        #   ssh-add = "ssh-add.exe";
         ls = "eza -la";
       };
       loginShellInit = ''
         export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-        '';
+      '';
     };
     programs.dconf.enable = true;
-    virtualisation.docker.enable = true;
     environment = {
       systemPackages = with pkgs; [
         emacs29
-	      coreutils
+        coreutils
         wsl-open
         wget
         curl
@@ -76,18 +75,16 @@ with lib; {
         hunspell
         hunspellDicts.en_CA
         (pkgs.writeScriptBin "update-system" ''
-                nix flake update ~/dotfiles/home-manager
-                sudo nixos-rebuild switch --flake ~/dotfiles/home-manager#cinnabar
-                '')
+          nix flake update ~/dotfiles/home-manager
+          sudo nixos-rebuild switch --flake ~/dotfiles/home-manager#cinnabar
+        '')
         (pkgs.writeScriptBin "update-home" ''
-                nix flake update ~/dotfiles/home-manager
-                home-manager switch --flake ~/dotfiles/home-manager
-                '')
+          nix flake update ~/dotfiles/home-manager
+          home-manager switch --flake ~/dotfiles/home-manager
+        '')
 
       ];
-      variables = rec {
-        BROWSER = "wsl-open";
-      };
+      variables = rec { BROWSER = "wsl-open"; };
     };
     system.stateVersion = "23.11";
   };
